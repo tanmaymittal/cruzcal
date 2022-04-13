9a2861f4a18ea1c1ac47408965235d9117a6df91
Pablo Gaeta <pgaeta@ucsc.edu>
2022-04-13 13:19:11 -0700

    Finished app skeleton

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,5 @@
+const db = require('./db');
 
-exports.getTerms = async (req, res) => {
-
-};
-
-exports.genSchedule = async (req, res) => {
-
-};
-
-exports.genCalendar = async (req, res) => {
-
-};
+// exports.getTerms = async (req, res) => {};
+// exports.genSchedule = async (req, res) => {};
+// exports.genCalendar = async (req, res) => {};
