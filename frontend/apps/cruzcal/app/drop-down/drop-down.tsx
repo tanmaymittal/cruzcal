c1915d6a496a0c505e193d484deee96a4b1e7dcf
Andrew Lim <aslimmax@yahoo.com>
2022-04-21 17:23:52 -0700

    Altered apperance on black background

--- frontend/apps/cruzcal/app/drop-down/drop-down.tsx
+++ frontend/apps/cruzcal/app/drop-down/drop-down.tsx
@@ -15,8 +15,8 @@ export const DropDown: FC<DropDownProps> = (props) => {
       {({ active }) => (
           <button
             className={`${
-              active ? 'bg-violet-500 text-white' : 'text-gray-900'
-            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+              active ? 'bg-violet-500 text-white' : 'text-gray-900 bg-white'
+            } group flex rounded-md items-center text-left w-full px-2 py-2 text-sm`}
           >
             {item}
           </button>
@@ -28,10 +28,10 @@ export const DropDown: FC<DropDownProps> = (props) => {
   const menuItems = props.items.map(createMenuItem);
 
   return (
-    <div className="w-56 text-right fixed top-16">
+    <div className="text-center top-16 bg-white">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-black bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-black bg-white rounded-md hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             {props.name}
             <ChevronDownIcon
               className="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
