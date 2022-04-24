f9018f4006a2c0f574a38285def4f23cc17302d9
Andrew Lim <aslimmax@yahoo.com>
2022-04-24 16:15:24 -0700

    Changed select icon

--- frontend/apps/cruzcal/app/select-list/select-list.tsx
+++ frontend/apps/cruzcal/app/select-list/select-list.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { CheckIcon, ChevronDownIcon, SelectorIcon } from '@heroicons/react/solid'
 
 // type Person = {
 //   name: string;
@@ -28,11 +28,11 @@ export const SelectList: FC<SelectListProps> = ({ listName, listOptions }) => {
     <div>
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
             <span className="block truncate">{selected.name}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <SelectorIcon
-                className="w-5 h-5 text-gray-400"
+              <ChevronDownIcon
+                className="w-5 h-5 ml-2 -mr-1 text-black"
                 aria-hidden="true"
               />
             </span>
