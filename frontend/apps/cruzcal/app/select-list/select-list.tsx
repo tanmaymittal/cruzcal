b09fd5d656fbd3c25ec4b00009cd518d8487e065
Andrew Lim <aslimmax@yahoo.com>
2022-04-24 01:48:47 -0700

    Modified styling

--- frontend/apps/cruzcal/app/select-list/select-list.tsx
+++ frontend/apps/cruzcal/app/select-list/select-list.tsx
@@ -25,9 +25,9 @@ export const SelectList: FC<SelectListProps> = ({ listName, listOptions }) => {
   const [selected, setSelected] = useState(listOptions[0])
 
   return (
-    <div className="w-72 top-16">
+    <div>
       <Listbox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
+        <div className="relative">
           <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
             <span className="block truncate">{selected.name}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
