d04fa6ddef44b43425b061c642f65ceafbc5b512
Pablo Gaeta <pgaeta@ucsc.edu>
2022-04-27 13:29:38 -0700

    Basic oauth working

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,64 @@
 const express = require('express');
+const session = require('express-session');
+const path = require('path');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const OpenApiValidator = require('express-openapi-validator');
+const passport = require('passport');
 
 const routes = require('./routes');
+const auth = require('./auth');
 
 // Initialize app
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '..', 'static')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Passport session setup
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 // Setup API validation
 
-const api = require('../api/openapi.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(api));
-app.use(
-  OpenApiValidator.middleware({
-    apiSpec: api,
-    validateRequests: true,
-    validateResponses: true,
-  }),
-);
+// const api = require('../api/openapi.json');
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(api));
+// app.use(
+//   OpenApiValidator.middleware({
+//     apiSpec: api,
+//     validateRequests: true,
+//     validateResponses: true,
+//   }),
+// );
 
 // Routes
 
+app.get('/auth/google', passport.authenticate(auth.googleStrategy));
+app.get('/auth/google/redirect',
+  passport.authenticate(auth.googleStrategy, {
+    failureRedirect: '/',
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect('/profile'));
+app.post('/logout', auth.logOut);
 app.get('/terms', routes.getTerms);
 app.post('/schedule', routes.genSchedule);
 app.post('/calendar', routes.genCalendar);
---
+++ backend/src/auth.js
@@ -0,0 +1,30 @@
+// const {getAllTerms, getTermByCode, getCourseByID} = require('./db');
+const GoogleStrategy = require('passport-google-oidc');
+
+exports.googleStrategy = new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/redirect',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/calendar',
+    ],
+  },
+  function verify(issuer, profile, cb) {
+    const {emails} = profile;
+    const ucscEmails = emails.filter((e) => e.value.endsWith('ucsc.edu'));
+    const ucscError = ucscEmails.length === 0;
+    console.log(`Attends UCSC? ${!ucscError}`);
+    console.log(profile);
+    if (ucscError) {
+      cb('Authentication Error: User does not have a ucsc email');
+    } else cb(null, profile);
+  });
+
+
+exports.logOut = async (req, res) => {
+  req.logout();
+  res.redirect('/');
+};
