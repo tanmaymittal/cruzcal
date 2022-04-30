60030f2e5ed13688c63e36cf28fc68adeaf4ba15
Andrew Lim <aslimmax@yahoo.com>
2022-04-29 20:24:27 -0700

    Added 2 example classes and auto content height

--- frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
+++ frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
@@ -1,7 +1,6 @@
 import styles from './calendar-view.module.css';
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
-// import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from '@fullcalendar/timegrid';
 
@@ -16,20 +15,26 @@ export function CalendarView(props: CalendarViewProps) {
       initialView="timeGridWeek"
       headerToolbar={false}
       weekends={false}
+      contentHeight="auto"
       allDaySlot={false}
       // Times are in 24 hour format
       slotMinTime="08:00"
       slotMaxTime="22:00"
       events={[
         {
-          id: 1,
+          id: "cse120",
+          daysOfWeek: ['1', '3', '5'],
           title: 'CSE 120',
-          date: '2022-04-25',
-          // Date and Time are separated by T
-          // Without a 'Z' for UTC time, JS will use the Browser's Time Zone
-          start: '2022-04-25T13:30:00',
-          end: '2022-04-25T15:15:00'
+          startTime: '13:30',
+          endTime: '15:15',
         },
+        {
+          id: "cse130",
+          daysOfWeek: ['2', '4'],
+          title: 'CSE 130',
+          startTime: '12:00',
+          endTime: '13:45',
+        }
       ]}
     />
   );
