9c53c82f1c89f8988c2a07ac7e29319c4d0c57b8
Pablo Gaeta <pgaeta@ucsc.edu>
2022-05-07 00:19:00 -0700

    Final commit

--- frontend/apps/cruzcal/app/select-list/select-list.tsx
+++ frontend/apps/cruzcal/app/select-list/select-list.tsx
@@ -18,7 +18,7 @@ export interface SelectListProps {
 
 export const SelectList: FC<SelectListProps> = ({ selected, setSelected, listName, options }) => {
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selected} onChange={setSelected}> {/*  disabled={options.length === 0} */}
       <div className="relative">
         <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
           <span className="block truncate">{selected === null ? `Select ${listName}...` : selected.name}</span>
