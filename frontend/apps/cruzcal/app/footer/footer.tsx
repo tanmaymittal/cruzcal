b2dff312f415d4a94aa7a681541cc6be0e24f41f
Andrew Lim <aslimmax@yahoo.com>
2022-05-15 01:10:33 -0700

    Created Header and Footer components

---
+++ frontend/apps/cruzcal/app/footer/footer.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FooterProps {}
+
+export function Footer(props: FooterProps) {
+  return (
+    <p className="text-center text-gray-500 text-xs">
+      &copy;2022 CruzCal. All rights reserved.
+    </p>
+  )
+}
+
+export default Footer;
---
+++ frontend/apps/cruzcal/app/header/header.tsx
@@ -0,0 +1,22 @@
+import UserHeader from '../user-header/UserHeader';
+
+/* eslint-disable-next-line */
+export interface HeaderProps {}
+
+export function Header(props: HeaderProps) {
+  return (
+    <div className="mb-10 text-white">
+      <div className="flex justify-center items-center">
+        <h1 className="text-7xl mb-2">CruzCal</h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <p className="text-xl mb-2">All your classes. One calendar file.</p>
+      </div>
+      <div className='flex flex-row-reverse'>
+        <UserHeader />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
