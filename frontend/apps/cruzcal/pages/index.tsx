077eae6c57c8df8e76c63f5c0aba838f6386f1ce
Andrew Lim <aslimmax@yahoo.com>
2022-05-15 01:12:15 -0700

    Reorganized layout

--- frontend/apps/cruzcal/pages/index.tsx
+++ frontend/apps/cruzcal/pages/index.tsx
@@ -2,52 +2,31 @@
 /* Components */
 import CalendarView from '../app/calendar-view/calendar-view';
 import CourseSelectionList from '../app/course-selection/CourseSelectionList';
-import UserHeader from '../app/user-header/UserHeader';
+import Header from '../app/header/header';
+import Footer from '../app/footer/footer';
 import WarningDialog from '../app/warning-dialog/warning-dialog';
-
-const PageHeader = () => {
-  return (
-    <div className="mb-10 text-white">
-      <div className="flex justify-center items-center">
-        <h1 className="text-7xl mb-2">CruzCal</h1>
-      </div>
-      <div className="flex justify-center items-center">
-        <p className="text-xl mb-2">All your classes. One calendar file.</p>
-      </div>
-      <div className='flex flex-row-reverse'>
-        <UserHeader />
-      </div>
-    </div>
-  )
-};
-
-const CalendarViewBody = () => {
-  return (
-    <div className="basis-3/5 border-solid border-2 border-white text-white">
-      <CalendarView />
-    </div>
-  )
-};
-
-const PageFooter = () => {
-  return (
-    <p className="text-center text-gray-500 text-xs">
-      &copy;2022 CruzCal. All rights reserved.
-    </p>
-  )
-};
-
 
 export function Index() {
   return (
-    <div className="container mx-auto p-3">
-      <PageHeader />
-      <div className="flex flex-col md:flex-row gap-x-14 mb-5">
-        <CalendarViewBody />
-        <CourseSelectionList />        
+    <>
+      <div className="container mx-auto">
+        <Header />
       </div>
-      <PageFooter />
-    </div>
+      <div className="container mx-auto p-3">
+        <div className="flex flex-col md:flex-row gap-x-14 mb-5">
+          <div className="basis-3/5 border-solid border-2 border-white text-white">
+            <CalendarView />
+          </div>
+          <div className="basis-2/5">
+            <CourseSelectionList />        
+          </div>
+        </div>
+      </div>    
+      <div className="container mx-auto">
+        <Footer />
+      </div>
+    </>
+
   );
 }
 
