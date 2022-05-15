31ee446cf129f6c35892fff5919a3eb2522d194b
Andrew Lim <aslimmax@yahoo.com>
2022-05-15 01:35:25 -0700

    Moved container class to components

--- frontend/apps/cruzcal/app/footer/footer.tsx
+++ frontend/apps/cruzcal/app/footer/footer.tsx
@@ -3,9 +3,11 @@ export interface FooterProps {}
 
 export function Footer(props: FooterProps) {
   return (
-    <p className="text-center text-gray-500 text-xs">
-      &copy;2022 CruzCal. All rights reserved.
-    </p>
+    <div className="container">
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2022 CruzCal. All rights reserved.
+      </p>      
+    </div>
   )
 }
 
--- frontend/apps/cruzcal/pages/index.tsx
+++ frontend/apps/cruzcal/pages/index.tsx
@@ -9,24 +9,19 @@ import WarningDialog from '../app/warning-dialog/warning-dialog';
 export function Index() {
   return (
     <>
-      <div className="container mx-auto">
-        <Header />
-      </div>
+      <Header />
       <div className="container mx-auto p-3">
         <div className="flex flex-col md:flex-row gap-x-14 mb-5">
           <div className="basis-3/5 border-solid border-2 border-white text-white">
             <CalendarView />
           </div>
           <div className="basis-2/5">
-            <CourseSelectionList />        
+            <CourseSelectionList />
           </div>
         </div>
       </div>    
-      <div className="container mx-auto">
-        <Footer />
-      </div>
+      <Footer />
     </>
-
   );
 }
 
