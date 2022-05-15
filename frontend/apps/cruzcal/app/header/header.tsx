bf9733def33927169186245e966ac4af88e0c2a3
Andrew Lim <aslimmax@yahoo.com>
2022-05-15 01:52:20 -0700

    Changed layout and right aligned logo

--- frontend/apps/cruzcal/app/header/header.tsx
+++ frontend/apps/cruzcal/app/header/header.tsx
@@ -5,18 +5,12 @@ export interface HeaderProps {}
 
 export function Header(props: HeaderProps) {
   return (
-    <div className="container">
-      <div className="mb-10 text-white">
-        <div className="flex justify-center items-center">
-          <h1 className="text-7xl mb-2">CruzCal</h1>
-        </div>
-        <div className="flex justify-center items-center">
-          <p className="text-xl mb-2">All your classes. One calendar file.</p>
-        </div>
-        <div className='flex flex-row-reverse'>
-          <UserHeader />
-        </div>
-      </div>      
+    <div className="container mx-auto p-3">
+      <h1 className="text-white text-7xl mb-2">CruzCal</h1>
+      <p className="text-white text-xl mb-2">All your classes. One calendar file.</p>
+      <div className='flex flex-row-reverse'>
+        <UserHeader />
+      </div>
     </div>
   )
 }
