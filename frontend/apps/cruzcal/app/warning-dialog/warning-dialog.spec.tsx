be9e3a6e59af49b9c033aeb538e2a2926be832bb
Kitana <kittoft@gmail.com>
2022-05-18 16:21:16 -0700

    fixed warningDialog jest test

--- frontend/apps/cruzcal/app/warning-dialog/warning-dialog.spec.tsx
+++ frontend/apps/cruzcal/app/warning-dialog/warning-dialog.spec.tsx
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { unmountComponentAtNode } from "react-dom";
 
