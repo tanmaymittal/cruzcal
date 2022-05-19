cf8e6c638fba2cc232cf00fc792160263e393607
Andrew Lim <aslimmax@yahoo.com>
2022-05-19 14:15:16 -0700

    Disabled server side rendering for CalendarView

--- frontend/apps/cruzcal/pages/index.tsx
+++ frontend/apps/cruzcal/pages/index.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 /* Components */
-import CalendarView from '../app/calendar-view/calendar-view';
 import CourseSelectionList from '../app/course-selection/CourseSelectionList';
 import Header from '../app/header/header';
 import Footer from '../app/footer/footer';
 import WarningDialog from '../app/warning-dialog/warning-dialog';
+import dynamic from 'next/dynamic'
+
+const Calendar = dynamic(
+  () => import('../app/calendar-view/calendar-view'),
+  { ssr: false}
+)
 
 export function Index() {
   return (
@@ -13,7 +18,7 @@ export function Index() {
       <div className="container mx-auto p-3">
         <div className="flex flex-col md:flex-row gap-x-14 mb-5">
           <div className="basis-2/3 border-solid border-2 border-white rounded-lg text-white">
-            <CalendarView />
+            <Calendar />
           </div>
           <div className="basis-1/3">
             <CourseSelectionList />
