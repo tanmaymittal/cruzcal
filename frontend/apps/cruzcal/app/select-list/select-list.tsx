f585bbf51302c42e79a4d747bc1223bb8905b18f
Andrew Lim <aslimmax@yahoo.com>
2022-05-19 12:26:01 -0700

    Condiitonal styling based on what's selected

--- frontend/apps/cruzcal/app/select-list/select-list.tsx
+++ frontend/apps/cruzcal/app/select-list/select-list.tsx
@@ -21,7 +21,11 @@ export const SelectList: FC<SelectListProps> = ({ selected, setSelected, listNam
   return (
     <Listbox value={selected} onChange={setSelected} disabled={disabled}>
       <div className="relative">
-        <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+        <Listbox.Button 
+          className={({disabled}) => 
+            `relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default ${disabled ? "bg-gray-400" : "hover:bg-gray-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500"} sm:text-sm`
+          }
+        >
           <span className="block truncate">{selected === null ? `Select ${listName}...` : selected.name}</span>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
             <ChevronDownIcon
