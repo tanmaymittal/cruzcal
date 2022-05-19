f7a1e4e770e7429241747b03c3c7a54b0157b363
Andrew Lim <aslimmax@yahoo.com>
2022-05-19 12:34:34 -0700

    Re-enabled CalendarView

--- frontend/apps/cruzcal/pages/index.tsx
+++ frontend/apps/cruzcal/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
       <div className="container mx-auto p-3">
         <div className="flex flex-col md:flex-row gap-x-14 mb-5">
           <div className="basis-2/3 border-solid border-2 border-white rounded-lg text-white">
-            {/* <CalendarView /> */}
+            <CalendarView />
           </div>
           <div className="basis-1/3">
             <CourseSelectionList />
