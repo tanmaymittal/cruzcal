5a67c1989d7a0c6a6d9cc4eb3db7ce0798878fab
Kitana <kittoft@gmail.com>
2022-05-20 14:27:59 -0700

    mocking data correcltly now

--- frontend/apps/cruzcal/app/warning-dialog/warning-dialog.spec.tsx
+++ frontend/apps/cruzcal/app/warning-dialog/warning-dialog.spec.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { unmountComponentAtNode } from "react-dom";
-// import { atom, useSetAtom } from 'jotai';
-// import { useUpdateAtom } from 'jotai/utils';
+import { Provider, useSetAtom } from 'jotai';
 
 import WarningDialog from './warning-dialog';
-// import { courseSelectionsAtom } from '../../atoms/course-selector';
 
-// import { CourseSelector } from '../../atoms/course-selector';
+import { courseSelectionsAtom } from '../../atoms/course-selector';
+import { CourseSelector } from '../../atoms/course-selector';
 
 let container = null;
-// const setCSelections = useSetAtom(courseSelectionsAtom);
+const setCSelections = useSetAtom(courseSelectionsAtom);
 
 beforeEach(() => {
   container = document.createElement("div");
@@ -26,89 +25,103 @@ afterEach(() => {
 
 describe('WarningDialog', () => {
   it('should render successfully', async () => {
+    // test 1: the warning dialog should NOT appear
     act(() => {
-      render(<WarningDialog />, container);
+      render(
+        <Provider>
+          <WarningDialog />
+        </Provider>, container);
+    })
+    expect(container).toBeTruthy();
+
+    // add another test to scan the document to ensure that the warning component is not on the document
+
+    const updatedCourses: CourseSelector[] = [
+      // Course 1
+      {
+        "term": {
+          "code": 2224,
+          "name": "2022 Summer Quarter",
+          "date": {
+            "end": "08/26/22",
+            "start": "07/25/22",
+          }
+
+        },
+        "subject": {
+          "name": "ANTH",
+        },
+
+        "course": {
+          "name": "Intr Culturl Anthro",
+          "professor": "Evans,D.N.",
+          "coursenum": "2",
+          "courseID": 70299,
+          "lectures": [{
+            "location": "Engineer 2 194",
+            "times": [{
+              "day": "Tuesday",
+              "start": "13:00",
+              "end": "16:30",
+            },
+            {
+              "day": "Thursday",
+              "start": "13:00",
+              "end": "16:30",
+            }]
+          }]
+        },
+      },
+
+      // Course 2
+      {
+        "term": {
+          "code": 2224,
+          "name": "2022 Summer Quarter",
+          "date": {
+            "end": "08/26/22",
+            "start": "07/25/22",
+          }
+
+        },
+        "subject": {
+          "name": "AM",
+        },
+
+        "course": {
+          "name": "Math Methods II",
+          "professor": "str",
+          "coursenum": "str",
+          "courseID": 70312,
+          "lectures": [{
+            "location": "Engineer 2 192",
+            "times": [{
+              "day": "Tuesday",
+              "start": "13:00",
+              "end": "16:30",
+            },
+            {
+              "day": "Thursday",
+              "start": "13:00",
+              "end": "16:30",
+            }]
+          }]
+        },
+      },
+    ];
+
+    setCSelections(updatedCourses);
+
+    // test 2: the warning dialog should appear: conflicts AM 2 & ANTHRO intro
+    act(() => {
+      render(
+        <Provider>
+          <WarningDialog />
+        </Provider>, container);
     })
 
-    // const updatedCourses: CourseSelector[] = [
-    //   // Course 1
-    //   {
-    //     "term": {
-    //       // http://localhost:3010/api/courses?term=2224&subject=ANTH
-    //       // {"code":"2224","date":{"end":"08/26/22","start":"07/25/22"},"name":"2022 Summer Quarter"}
-    //   "code": 2224,
-    //   "name": "2022 Summer Quarter",
-    //   "date": {
-    //     "end": "08/26/22",
-    //     "start": "07/25/22",
-    //     }
-
-    //     },
-    //     "subject": {
-    //       "name": "ANTH",
-    //     },
-
-    //     "course": {
-    //       "name":	"Intr Culturl Anthro",
-    //       "professor": "Evans,D.N.",
-    //       "coursenum": "2",
-    //       "courseID": 70299,
-    //       "lectures": {
-    //         "location": "Engineer 2 194",
-    //         "times": [{
-    //           "day": "Tuesday",
-    //           "start": "13:00",
-    //           "end": "16:30",
-    //         },
-    //           {
-    //           "day": "Thursday",
-    //           "start": "13:00",
-    //           "end": "16:30",
-    //         }]
-    //       }
-    //     },
-    //   },
-
-    //   // Course 2
-    //   {
-    //     "term": {
-    //       // http://localhost:3010/api/courses?term=2224&subject=AM
-    //       // {"code":"2224","date":{"end":"08/26/22","start":"07/25/22"},"name":"2022 Summer Quarter"}
-    //   "code": 2224,
-    //   "name": "2022 Summer Quarter",
-    //   "date": {
-    //     "end": "08/26/22",
-    //     "start": "07/25/22",
-    //     }
-
-    //     },
-    //     "subject": {
-    //       "name": "AM",
-    //     },
-
-    //     "course": {
-    //       "name":	"Math Methods II",
-    //       "professor": "str",
-    //       "coursenum": "str",
-    //       "courseID": 70312,
-    //       "lectures": {
-    //         "location": "Engineer 2 192",
-    //         "times": [{
-    //           "day": "Tuesday",
-    //           "start": "13:00",
-    //           "end": "16:30",
-    //         },
-    //           {
-    //           "day": "Thursday",
-    //           "start": "13:00",
-    //           "end": "16:30",
-    //         }]
-    //       }
-    //     },
-    //   },
-    // ];
-    // setCSelections(updatedCourses);
-
+    // validate that the warningDialog has the two conflicting tests in it
     expect(container).toBeTruthy();
+    // check data that I expect is here
   });
 });
