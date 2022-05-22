a2e18711c47e192f5d5ffeb24a9fdde8de35cca6
Andrew Lim <aslimmax@yahoo.com>
2022-05-22 15:37:03 -0700

    Al changes made in original calendar-view branch

--- frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
+++ frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
@@ -5,14 +5,47 @@ import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from '@fullcalendar/timegrid';
 import momentPlugin from '@fullcalendar/moment'
 
+import { useAtom } from 'jotai';
+import { courseSelectionsAtom, CourseSelector } from '../../atoms/course-selector';
+import { core } from 'apps/cruzcal/.storybook/main';
 
 /* eslint-disable-next-line */
-export interface CalendarViewProps {}
+export interface CalendarViewProps { }
+
+const weekdayNum = (name) => ({
+  'Monday': '1',
+  'Tuesday': '2',
+  'Wednesday': '3',
+  'Thursday': '4',
+  'Friday': '5',
+})[name] || console.error(`Weekday does not exist: ${name}`);
 
 export function CalendarView(props: CalendarViewProps) {
-  const header = {start: '', center: 'title', end: ''}
+  const [courseListAtoms, dispatch] = useAtom(courseSelectionsAtom);
+  const header = { start: '', center: 'title', end: '' }
+
+  const courseListValid = courseListAtoms.filter(
+    ({ course, subject, term }) => (
+      course && subject && term && course.lectures[0].times.length > 0
+    )
+  );
+
   return (
     <FullCalendar
+      events={courseListValid.map((courseAtom, i) => {
+        const courseInfo = courseAtom.course
+        const lectureTimes = courseInfo.lectures[0].times;
+
+        const courseLectureDays = lectureTimes.map(({ day }) => weekdayNum(day));
+
+        return {
+          id: `${i}`,
+          daysOfWeek: courseLectureDays as string[],
+          title: courseInfo.name as string,
+          startTime: lectureTimes[0].start as string,
+          endTime: lectureTimes[0].end as string,
+        }
+      })}
       plugins={[momentPlugin, timeGridPlugin]}
       allDaySlot={false}
       contentHeight="auto"
@@ -24,22 +57,6 @@ export function CalendarView(props: CalendarViewProps) {
       // Times are in 24 hour format
       slotMinTime="08:00"
       slotMaxTime="22:00"
-      events={[
-        {
-          id: "cse120",
-          daysOfWeek: ['1', '3', '5'],
-          title: 'CSE 120',
-          startTime: '13:30',
-          endTime: '15:15',
-        },
-        {
-          id: "cse130",
-          daysOfWeek: ['2', '4'],
-          title: 'CSE 130',
-          startTime: '12:00',
-          endTime: '13:45',
-        }
-      ]}
     />
   );
 }
