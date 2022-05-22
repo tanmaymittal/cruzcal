e8df3c7f0ac576757dc5daaac7a6022cf672c06f
Pablo Gaeta <pablofgaeta@gmail.com>
2022-05-21 23:36:47 -0700

    Fixed auth popup to not login/signup user

--- frontend/apps/cruzcal/app/course-selection/CourseSelection.tsx
+++ frontend/apps/cruzcal/app/course-selection/CourseSelection.tsx
@@ -13,11 +13,7 @@ const warningWrapper = (warnings, selection) =>{
   const baseClasses = ['mb-5', 'w-full', 'flex', 'gap-x-3'];
   // check if your current course name exists in any of the warnings
   for (let i = 0; i < warnings.length; i++) {
-    if (selection.course == null || selection.subject == null || selection.term == null) {
-      break;
-    }
-
-    if (selection.course.name == warnings[i].course.name) {
+    if (selection?.course?.name == warnings[i].course.name) {
       return classnames(...baseClasses, "border-2", "border-rose-500");
     }
   }
