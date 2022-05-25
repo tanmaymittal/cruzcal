b42a76adbdd197c1a3d6d8bf14e35facee3b75d2
Pablo Gaeta <pablofgaeta@gmail.com>
2022-05-25 13:52:58 -0700

    Dynamic date and cleanup calendar
    Set the date for the term to be set to the current date to help with testing failures (always the current date).
    
    Added a cleanup section to remove the generated google calendar after creation.

--- backend/src/__tests__/app/calendar.test.js
+++ backend/src/__tests__/app/calendar.test.js
@@ -1,31 +1,47 @@
 /* eslint-disable max-len */
 require('dotenv').config();
-const {addGoogleCalApiEvents, generateNameForCalendarId} = require('../../calendar');
+const {addGoogleCalApiEvents, genNameForCalendarSummary, formatDateString, deleteCalendar} = require('../../calendar');
 const {coursesData} = require('../common');
+
+const date = new Date();
+const calendarDate = formatDateString({
+  year: date.getFullYear(),
+  month: date.getMonth() + 1,
+  date: date.getDate(),
+});
 
 const termData = {
   'code': 2222,
   'name': '1234 Test Quarter',
   'date': {
-    'start': '2022-05-25',
-    'end': '2022-05-25',
+    'start': calendarDate,
+    'end': calendarDate,
   },
 };
 
 describe('Calendar functions properly generate events along helper functions', () => {
+  const calendarSummary = genNameForCalendarSummary(
+    termData,
+    coursesData,
+  );
+
+  test('Function genNameForCalendarSummary creates unique name for calendar', () => {
+    expect(calendarSummary).toBe('1234 Test Quarter: Math Methods II, SOE Calculus III');
+  });
   test('Function addGoogleCalApiEvents creates google calendar events', async () => {
-    const events = await addGoogleCalApiEvents(
+    const {calendarId, courseEvents: events} = await addGoogleCalApiEvents(
       process.env.GOOGLE_TEST_TOKEN,
       termData,
       coursesData,
     );
     expect(events.length).toBe(2);
-  });
-  test('Function generateNameForCalendarId creates unique name for calendar', async () => {
-    const calendarId = await generateNameForCalendarId(
-      termData,
-      coursesData,
-    );
-    expect(calendarId).toBe('1234 Test Quarter: Math Methods II, SOE Calculus III');
+
+    // Cleanup calendar after test
+    try {
+      await deleteCalendar(process.env.GOOGLE_TEST_TOKEN, calendarId);
+    } catch (error) {
+      console.error(error);
+      expect(error).toBeFalsy(); // throw an error
+    }
   });
 });
