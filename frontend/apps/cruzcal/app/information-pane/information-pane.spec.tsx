399e34058fdd14879b61524530e80be4f92cac39
Kitana <kittoft@gmail.com>
2022-05-26 16:19:11 -0700

    see warnings --> see schedule conflicts

--- frontend/apps/cruzcal/app/information-pane/information-pane.spec.tsx
+++ frontend/apps/cruzcal/app/information-pane/information-pane.spec.tsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import InformationPane from './information-pane';
 
 describe('InformationPane', () => {
-  it('should render successfully', () => {
+  it('should render successfully', async () => {
     const { baseElement } = render(<InformationPane />);
     expect(baseElement).toBeTruthy();
   });
