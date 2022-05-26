87d736d2e2b502d1048f946c9a0bc5d6264d3597
Pablo Gaeta <pablofgaeta@gmail.com>
2022-05-26 16:03:26 -0700

    Added course selection search query when schedule changes

--- frontend/apps/cruzcal/atoms/course-selector.ts
+++ frontend/apps/cruzcal/atoms/course-selector.ts
@@ -1,9 +1,10 @@
 import { atom } from "jotai";
 import { atomWithHash, atomWithStorage, splitAtom } from "jotai/utils";
+import { server } from "../config";
 import { CourseInfo } from "./courses";
 import selectedTermAtom from "./selected-term";
 import { SubjectInfo } from "./subjects";
-import { TermInfo } from "./terms";
+import termsAtom, { TermInfo } from "./terms";
 
 export interface CourseSelector {
   term: TermInfo,
@@ -25,33 +26,46 @@ const initialScheduleSelections = {
 export const courseSelectionsStorageAtom = atomWithStorage('course-selector', [{...defaultCourseSelection}]);
 export const courseSelectionsAtom = atom(
   (get) => get(courseSelectionsStorageAtom),
-  (get, set, courseSelections) => {
+  (get, set, courseSelections: CourseSelector[]) => {
     set(courseSelectionsStorageAtom, courseSelections);
-    // set(scheduleSelectionsAtom, {...get(scheduleSelectionsAtom), courses: courseSelections});
   },
 );
+courseSelectionsAtom.onMount = ((setAtom) => {
+  console.log(location.search);
+})
 export const courseSelectionAtomsAtom = splitAtom(courseSelectionsAtom);
 
 export const multipleCourseSelectionsAtom = atom((get) => get(courseSelectionAtomsAtom).length > 1);
 
-// export const scheduleHashAtom = atomWithHash('schedule', initialScheduleSelections, {
-//   replaceState: true,
-// });
+export const generateScheduleURI = (term: TermInfo, courses: CourseInfo[]) => {
+  const completedCourses = courses.filter((course) => course?.courseID != null);
 
-// export const generateScheduleQuery = (type: string, term: TermInfo, courses: CourseInfo[]) => {
-//   const termCodeStr = term === null ? '' : `termCode=${encodeURIComponent(term.code)}`;
-//   const courseIDsStr = courses
-//     .map((course) => course === null ? '' : `courseIDs=${encodeURIComponent(course.courseID)}`)
-//     .join('&');
-//   console.log(courseIDsStr);
-//   return `?${termCodeStr}${courseIDsStr}`;
-// };
+  if (term == null) {
+    throw new Error(`Course selection incomplete`);
+  } else if (completedCourses.length === 0) {
+    throw new Error('No courses selected');
+  }
+
+  const termCodeStr = `termCode=${encodeURIComponent(term.code)}`;
+  const courseIDsStr = completedCourses
+    .map((course) => `courseIDs=${encodeURIComponent(course.courseID)}`)
+    .join('&');
+  return `${location.pathname}?${termCodeStr}&${courseIDsStr}`;
+};
 
 export const scheduleSelectionsAtom = atom(
-  (get) => ({
-    term: get(selectedTermAtom),
-    courses: get(courseSelectionsAtom)
-  }), (get, set, newTermAndCS: {term: TermInfo, courses: CourseSelector[]}) => {
+  (get) => {
+    const term = get(selectedTermAtom);
+    const courseSelections = get(courseSelectionsAtom);
+    try {
+      const courses = courseSelections.map(({course}) => course);
+      const scheduleURI = generateScheduleURI(term, courses);
+      history.pushState(null, '', scheduleURI);
+    } catch (error) {
+      console.log(error.message);
+    }
+    return {term, courses: courseSelections};
+  }, (get, set, newTermAndCS: {term: TermInfo, courses: CourseSelector[]}) => {
     // const {term, courses} = newTermAndCS;
     // history.pushState(newTermAndCS, "Schedule Selection", generateScheduleQuery('', term, courses.map(({course}) => course)));
     // set(scheduleHashAtom, newTermAndCS);
