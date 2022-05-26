e1ae1e569ed76451b19f107a95f1bc2c085a9946
Andrew Lim <aslimmax@yahoo.com>
2022-05-25 23:11:18 -0700

    Reorganized components

--- frontend/apps/cruzcal/pages/index.tsx
+++ frontend/apps/cruzcal/pages/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-/* Components */
+import { useUpdateAtom } from 'jotai/utils';
+import { useEffect } from 'react';
+
 import CourseSelectionList from '../app/course-selection/CourseSelectionList';
 import Header from '../app/header/header';
 import Footer from '../app/footer/footer';
-import dynamic from 'next/dynamic'
 import { versionAtom } from '../atoms/version';
-import { useUpdateAtom } from 'jotai/utils';
-import { useEffect } from 'react';
+import dynamic from 'next/dynamic'
+import Mobile from './mobile';
+import Desktop from './desktop';
 
 const Calendar = dynamic(
   () => import('../app/calendar-view/calendar-view'),
@@ -23,16 +25,8 @@ export function Index() {
   return (
     <>
       <Header />
-      <div className="container mx-auto p-3">
-        <div className="flex flex-col md:flex-row gap-x-14 mb-5">
-          <div className="basis-2/3 border-solid border-2 border-white rounded-lg text-white">
-            <Calendar />
-          </div>
-          <div className="basis-1/3">
-            <CourseSelectionList />
-          </div>
-        </div>
-      </div>    
+      <Mobile />
+      <Desktop />
       <Footer />
     </>
   );
