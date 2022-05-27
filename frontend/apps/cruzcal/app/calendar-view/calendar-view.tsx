121a94510cce320f4e2d2aac957b2cff6616e713
Andrew Lim <aslimmax@yahoo.com>
2022-05-26 21:49:04 -0700

    Dynamic calendar title based on selected term

--- frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
+++ frontend/apps/cruzcal/app/calendar-view/calendar-view.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import timeGridPlugin from '@fullcalendar/timegrid';
 import momentPlugin from '@fullcalendar/moment'
 
 import { useAtomValue } from 'jotai';
 import { courseSelectionsAtom, CourseSelector } from '../../atoms/course-selector';
+import selectedTermAtom from "../../atoms/selected-term";
+import moment from 'moment'
 
 /* eslint-disable-next-line */
 export interface CalendarViewProps { }
@@ -49,6 +51,17 @@ const getCourseEvents = (courseList: CourseSelector[]) => {
 export function CalendarView(props: CalendarViewProps) {
   const courseList: CourseSelector[] = useAtomValue(courseSelectionsAtom);
   const header = { start: '', center: 'title', end: '' }
+  const selectedTerm = useAtomValue(selectedTermAtom)
+
+  useEffect(() => {
+    const startDate = moment(selectedTerm.date.start, 'YYYY-MM-DD').format('MMM YYYY')
+    const endDate = moment(selectedTerm.date.end, 'YYYY-MM-DD').format('MMM YYYY')
+    const titles = document.getElementsByClassName('fc-toolbar-title')
+
+    for (let i = 0; i < titles.length; i++) {
+      titles[i].innerHTML = startDate + ' - ' + endDate
+    }
+  }, [selectedTerm])
 
   return (
     <FullCalendar
