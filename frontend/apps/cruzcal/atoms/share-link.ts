794a33c7d36a6c2be2ee090d0c9b7b1844dcb86c
Pablo Gaeta <pablofgaeta@gmail.com>
2022-05-28 15:24:19 -0700

    separated schedule uri/query generation

--- frontend/apps/cruzcal/atoms/share-link.ts
+++ frontend/apps/cruzcal/atoms/share-link.ts
@@ -25,28 +25,27 @@ export const fetchSchedule = async (scheduleQuery) => {
       }))
     }
   }
-} 
+}
 
-export const generateScheduleURI = ({term, courses}: CourseSchedule) => {
+export const generateScheduleQuery = ({term, courses}: CourseSchedule) => {
   const completedCourseIDs = courses
     .map(({course}) => course?.courseID)
     .filter((id) => id != null);
 
   if (term == null || completedCourseIDs.length === 0) {
-    return `${server}/`; // no schedule query
+    return ``; // no schedule query
   } 
 
   const termCodeStr = `termCode=${encodeURIComponent(term.code)}`;
   const courseIDsStr = completedCourseIDs
     .map((courseID) => `courseIDs=${encodeURIComponent(courseID)}`)
     .join('&');
-  return `${server}/?${termCodeStr}&${courseIDsStr}`;
+  return `?${termCodeStr}&${courseIDsStr}`;
 };
 
+export const generateScheduleURI = (schedule: CourseSchedule) => 
+  `${server}/${generateScheduleQuery(schedule)}`;
 
-export const shareLinkAtom = atom(
-  (get) => generateScheduleURI(get(scheduleSelectionAtom))
-)
-
+export const shareLinkAtom = atom((get) => generateScheduleURI(get(scheduleSelectionAtom)));
 
 export default shareLinkAtom;
