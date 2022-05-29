607f40e8a8198feeaa4141da3b431ae369212d67
Andrew Lim <aslimmax@gmail.com>
2022-05-29 00:08:19 -0700

    Removed all instances of font-medium

--- frontend/apps/cruzcal/app/warning-dialog/warning-dialog.tsx
+++ frontend/apps/cruzcal/app/warning-dialog/warning-dialog.tsx
@@ -62,7 +62,7 @@ export const WarningDialog = () => {
                 <Dialog.Overlay className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg leading-6 text-gray-900"
                   >
                     You may have scheduling conflicts.
                   </Dialog.Title>
@@ -80,7 +80,7 @@ export const WarningDialog = () => {
                   <div className="mt-4">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-rose-300 px-4 py-2 text-sm font-medium text-rose-700 hover:bg-rose-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-rose-300 px-4 py-2 text-sm text-rose-700 hover:bg-rose-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeModal}
                     >
                       Got it, thanks!
