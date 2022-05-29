6276b58bad9e262779ead9bf4c7de76b5dffff96
Andrew Lim <aslimmax@gmail.com>
2022-05-29 00:33:07 -0700

    Expand width of modal

--- frontend/apps/cruzcal/app/information-pane/information-pane.tsx
+++ frontend/apps/cruzcal/app/information-pane/information-pane.tsx
@@ -53,7 +53,7 @@ export const InformationPane = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-lg leading-6 text-gray-900"
